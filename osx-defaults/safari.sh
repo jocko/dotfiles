89555ee2a4b1913198c209b4a5651a3a0fbad107
Joakim Erelt <joakim.erelt@gmail.com>
2013-05-10 19:21:18 +0200

    Safari config

---
+++ osx-defaults/safari.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# New tabs open with empty page
+defaults write com.apple.Safari NewTabBehavior -int 1
+
+# Set Safari’s home page
+defaults write com.apple.Safari HomePage -string "www.google.com"
+
+# Prevent Safari from opening ‘safe’ files automatically after downloading
+defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
+
+# Enable Safari’s debug menu
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+
+# Enable the Develop menu and the Web Inspector in Safari
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
