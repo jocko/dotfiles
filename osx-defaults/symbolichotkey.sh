10b7b652fc52cca0463641429e3548de84dc8172
Joakim Erelt <joakim.erelt@gmail.com>
2013-05-10 18:11:56 +0200

    Hotkey config

---
+++ osx-defaults/symbolichotkey.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Disable: Mission Control, ctrl+up arrow
+#/usr/libexec/PlistBuddy -c "Set :AppleSymbolicHotKeys:32:enabled bool false" ~/Library/Preferences/com.apple.symbolichotkeys.plist
+
+# Disable: Show Help menu, shift+cmd+/
+/usr/libexec/PlistBuddy -c "Set :AppleSymbolicHotKeys:98:enabled bool false" ~/Library/Preferences/com.apple.symbolichotkeys.plist
+
+# Custom: Move focus to next window, cmd+§
+/usr/libexec/PlistBuddy -c "Delete :AppleSymbolicHotKeys:27" ~/Library/Preferences/com.apple.symbolichotkeys.plist
+/usr/libexec/PlistBuddy -c "Add :AppleSymbolicHotKeys:27 dict" ~/Library/Preferences/com.apple.symbolichotkeys.plist
+/usr/libexec/PlistBuddy -c "Merge cmd-paragraph-hotkey.plist :AppleSymbolicHotKeys:27 dict" ~/Library/Preferences/com.apple.symbolichotkeys.plist
