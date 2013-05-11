e7d001fb44ddebd9a16daef94c839e1c84e0bb4d
Joakim Erelt <joakim.erelt@gmail.com>
2013-05-11 23:55:07 +0200

    Show status bar

--- osx-defaults/safari.sh
+++ osx-defaults/safari.sh
@@ -16,3 +16,6 @@ defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
+
+# Show status bar
+defaults write com.apple.Safari ShowStatusBar -bool TRUE
