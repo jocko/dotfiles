a34896b34e567b644c58f297a43ce436eae818cf
Joakim Erelt <joakim.erelt@gmail.com>
2013-05-12 16:19:40 +0200

    Use 24-hour clock

--- osx-defaults/misc.sh
+++ osx-defaults/misc.sh
@@ -9,3 +9,6 @@ defaults write com.apple.dashboard mcx-disabled -bool true
 # Empirical studies have shown that these settings are optimal key repeat values
 defaults write -g InitialKeyRepeat -int 14
 defaults write -g KeyRepeat -int 2
+
+# Use a 24-hour clock
+defaults write com.apple.menuextra.clock DateFormat -string "EEE HH:mm"
