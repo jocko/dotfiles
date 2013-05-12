4260497b155f0e8ec1ed7db0794a692eb0ea655f
Joakim Erelt <joakim.erelt@gmail.com>
2013-05-12 15:31:07 +0200

    Use location variable where appliable

--- iterm2/iterm2-configure.sh
+++ iterm2/iterm2-configure.sh
@@ -14,8 +14,8 @@ defaults write com.googlecode.iterm2 PromptOnClose -int 0
 # Install the theme (Tomorrow Night)
 /usr/libexec/PlistBuddy -c "Delete :'Custom Color Presets':'Tomorrow Night'" $iterm2_plist 2>/dev/null
 set -e
-/usr/libexec/PlistBuddy -c "Add :'Custom Color Presets':'Tomorrow Night' dict" ~/Library/Preferences/com.googlecode.iterm2.plist
-/usr/libexec/PlistBuddy -c "Merge 'Tomorrow Night.itermcolors' :'Custom Color Presets':'Tomorrow Night'" ~/Library/Preferences/com.googlecode.iterm2.plist
+/usr/libexec/PlistBuddy -c "Add :'Custom Color Presets':'Tomorrow Night' dict" $iterm2_plist
+/usr/libexec/PlistBuddy -c "Merge 'Tomorrow Night.itermcolors' :'Custom Color Presets':'Tomorrow Night'" $iterm2_plist
 
 # Apply the theme (for the first profile)
 #
@@ -25,7 +25,7 @@ set -e
 # existing keys which means that we first have to delete all the current color
 # keys before we actually can apply the theme.
 for color in "Ansi 0 Color" "Ansi 1 Color" "Ansi 2 Color" "Ansi 3 Color" "Ansi 4 Color" "Ansi 5 Color" "Ansi 6 Color" "Ansi 7 Color" "Ansi 8 Color" "Ansi 9 Color" "Ansi 10 Color" "Ansi 11 Color" "Ansi 12 Color" "Ansi 13 Color" "Ansi 14 Color" "Ansi 15 Color" "Background Color" "Bold Color" "Cursor Color" "Cursor Text Color" "Foreground Color" "Selected Text Color" "Selection Color"; do
-  /usr/libexec/PlistBuddy -c "Delete :'New Bookmarks':0:'$color'" ~/Library/Preferences/com.googlecode.iterm2.plist
+  /usr/libexec/PlistBuddy -c "Delete :'New Bookmarks':0:'$color'" $iterm2_plist
 done
 
-/usr/libexec/PlistBuddy -c "Merge 'Tomorrow Night.itermcolors' :'New Bookmarks':0" ~/Library/Preferences/com.googlecode.iterm2.plist
+/usr/libexec/PlistBuddy -c "Merge 'Tomorrow Night.itermcolors' :'New Bookmarks':0" $iterm2_plist
