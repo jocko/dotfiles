21bec868031142664d169a1f09d6494ec0d188d9
Joakim Erelt <joakim.erelt@gmail.com>
2013-05-17 18:50:00 +0200

    Don't show all file name extensions in Finder

--- osx-defaults/finder.sh
+++ osx-defaults/finder.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Show all filename extensions
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+#defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
 # Show status bar
 defaults write com.apple.finder ShowStatusBar -bool true
