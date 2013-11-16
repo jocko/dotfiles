102397bac619f4b9ba11b771c0ccd179972f1a93
Joakim Erelt <joakim.erelt@gmail.com>
2013-11-16 10:23:09 +0100

    Make top left corner start screen saver

--- osx-defaults/dock.sh
+++ osx-defaults/dock.sh
@@ -12,12 +12,12 @@
 # 10: Put display to sleep
 # 11: Launchpad
 # 12: Notification Center
-# Top left screen corner → Mission Control
-defaults write com.apple.dock wvous-tl-corner -int 2
+# Bottom left screen corner → Start screen saver
+defaults write com.apple.dock wvous-tl-corner -int 5
 defaults write com.apple.dock wvous-tl-modifier -int 0
 # Bottom right screen corner → Desktop
 defaults write com.apple.dock wvous-br-corner -int 4
 defaults write com.apple.dock wvous-br-modifier -int 0
-# Bottom left screen corner → Start screen saver
-defaults write com.apple.dock wvous-bl-corner -int 5
+# Top left screen corner → Mission Control
+defaults write com.apple.dock wvous-bl-corner -int 2
 defaults write com.apple.dock wvous-bl-modifier -int 0
