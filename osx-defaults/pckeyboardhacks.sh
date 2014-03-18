00a7b118cc7f5cdd6606f20f05b242bde0f886b8
Joakim Erelt <joakim.erelt@gmail.com>
2014-03-18 20:20:36 +0100

    Remove some old files

--- osx-defaults/pckeyboardhacks.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Map caps lock to escape
-#
-# Note that when remapping caps lock, we should also remove the corresponding modifier 
-# key under System Preferences/Modifier Keys... (i.e. set Caps Lock to "No Action").
-defaults write org.pqrs.PCKeyboardHack sysctl -dict enable_capslock -bool TRUE keycode_capslock -int 53
--- osx-defaults/systemsound.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-defaults write com.apple.systemsound com.apple.sound.beep.volume -float 0
-#defaults write com.apple.systemsound com.apple.sound.uiaudio.enabled -int 0
-defaults write -g com.apple.sound.beep.feedback -int 0
