11763b37b4f3aa2ce173ac15b39671beba861bc4
Joakim Erelt <joakim.erelt@gmail.com>
2014-04-12 09:58:17 +0200

    Create half finished script for generating iTerm themes

---
+++ twilight.rb
@@ -0,0 +1,242 @@
+colors = {
+    'Ansi 0 Color' =>
+        {
+            'Blue Component' =>
+                '0.078821629285812378',
+            'Green Component' =>
+                '0.07883714884519577',
+            'Red Component' =>
+                '0.078830234706401825',
+        },
+    'Ansi 1 Color' =>
+        {
+            'Blue Component' =>
+                '0.26590591669082642',
+            'Green Component' =>
+                '0.42739877104759216',
+            'Red Component' =>
+                '0.7543104887008667',
+        },
+    'Ansi 10 Color' =>
+        {
+            'Blue Component' =>
+                '0.55018079280853271',
+            'Green Component' =>
+                '0.84882640838623047',
+            'Red Component' =>
+                '0.79963678121566772',
+        },
+    'Ansi 11 Color' =>
+        {
+            'Blue Component' =>
+                '0.49316486716270447',
+            'Green Component' =>
+                '0.76867014169692993',
+            'Red Component' =>
+                '0.88534402847290039',
+        },
+    'Ansi 12 Color' =>
+        {
+            'Blue Component' =>
+                '0.38419744372367859',
+            'Green Component' =>
+                '0.36815744638442993',
+            'Red Component' =>
+                '0.35298055410385132',
+        },
+    'Ansi 13 Color' =>
+        {
+            'Blue Component' =>
+                '0.55795210599899292',
+            'Green Component' =>
+                '0.86099404096603394',
+            'Red Component' =>
+                '0.81614553928375244',
+        },
+    'Ansi 14 Color' =>
+        {
+            'Blue Component' =>
+                '0.60617965459823608',
+            'Green Component' =>
+                '0.59707218408584595',
+            'Red Component' =>
+                '0.54136258363723755',
+        },
+    'Ansi 15 Color' =>
+        {
+            'Blue Component' =>
+                '0.83107829093933105',
+            'Green Component' =>
+                '1',
+            'Red Component' =>
+                '0.99986904859542847',
+        },
+    'Ansi 2 Color' =>
+        {
+            'Blue Component' =>
+                '0.47654882073402405',
+            'Green Component' =>
+                '0.72692984342575073',
+            'Red Component' =>
+                '0.68779230117797852',
+        },
+    'Ansi 3 Color' =>
+        {
+            'Blue Component' =>
+                '0.42418986558914185',
+            'Green Component' =>
+                '0.66052716970443726',
+            'Red Component' =>
+                '0.76260745525360107',
+        },
+    'Ansi 4 Color' =>
+        {
+            'Blue Component' =>
+                '0.28883245587348938',
+            'Green Component' =>
+                '0.27791988849639893',
+            'Red Component' =>
+                '0.26757293939590454',
+        },
+    'Ansi 5 Color' =>
+        {
+            'Blue Component' =>
+                '0.4848540723323822',
+            'Green Component' =>
+                '0.74553430080413818',
+            'Red Component' =>
+                '0.70590567588806152',
+        },
+    'Ansi 6 Color' =>
+        {
+            'Blue Component' =>
+                '0.52099674940109253',
+            'Green Component' =>
+                '0.51336991786956787',
+            'Red Component' =>
+                '0.46818926930427551',
+        },
+    'Ansi 7 Color' =>
+        {
+            'Blue Component' =>
+                '0.83107829093933105',
+            'Green Component' =>
+                '1',
+            'Red Component' =>
+                '0.99986904859542847',
+        },
+    'Ansi 8 Color' =>
+        {
+            'Blue Component' =>
+                '0.15053729712963104',
+            'Green Component' =>
+                '0.15056693553924561',
+            'Red Component' =>
+                '0.15055373311042786',
+        },
+    'Ansi 9 Color' =>
+        {
+            'Blue Component' =>
+                '0.29907804727554321',
+            'Green Component' =>
+                '0.48694252967834473',
+            'Red Component' =>
+                '0.86878842115402222',
+        },
+    'Background Color' =>
+        {
+            'Blue Component' =>
+                '0.078821629285812378',
+            'Green Component' =>
+                '0.07883714884519577',
+            'Red Component' =>
+                '0.078830234706401825',
+        },
+    'Bold Color' =>
+        {
+            'Blue Component' =>
+                '0.83107829093933105',
+            'Green Component' =>
+                '1',
+            'Red Component' =>
+                '0.99986904859542847',
+        },
+    'Cursor Color' =>
+        {
+            'Blue Component' =>
+                '1',
+            'Green Component' =>
+                '1',
+            'Red Component' =>
+                '1',
+        },
+    'Cursor Text Color' =>
+        {
+            'Blue Component' =>
+                '0.0',
+            'Green Component' =>
+                '0.0',
+            'Red Component' =>
+                '0.0',
+        },
+    'Foreground Color' =>
+        {
+            'Blue Component' =>
+                '0.83107829093933105',
+            'Green Component' =>
+                '1',
+            'Red Component' =>
+                '0.99986904859542847',
+        },
+    'Selected Text Color' =>
+        {
+            'Blue Component' =>
+                '0.83107829093933105',
+            'Green Component' =>
+                '1',
+            'Red Component' =>
+                '0.99986904859542847',
+        },
+    'Selection Color' =>
+        {
+            'Blue Component' =>
+                '0.1919473260641098',
+            'Green Component' =>
+                '0.1919851154088974',
+            'Red Component' =>
+                '0.19196827709674835',
+        },
+}
+
+
+names = {
+    'Ansi 7 Color' =>  'ansi_7_color',
+    'Selected Text Color' =>  'selected_text_color',
+    'Cursor Text Color' =>  'cursor_text_color',
+    'Ansi 15 Color' =>  'ansi_15_color',
+    'Ansi 2 Color' =>  'ansi_2_color',
+    'Ansi 8 Color' =>  'ansi_8_color',
+    'Ansi 11 Color' =>  'ansi_11_color',
+    'Bold Color' =>  'bold_color',
+    'Ansi 3 Color' =>  'ansi_3_color',
+    'Ansi 9 Color' =>  'ansi_9_color',
+    'Ansi 14 Color' =>  'ansi_14_color',
+    'Background Color' =>  'background_color',
+    'Ansi 10 Color' =>  'ansi_10_color',
+    'Ansi 4 Color' =>  'ansi_4_color',
+    'Cursor Color' =>  'cursor_color',
+    'Ansi 13 Color' =>  'ansi_13_color',
+    'Ansi 5 Color' =>  'ansi_5_color',
+    'Ansi 0 Color' =>  'ansi_0_color',
+    'Ansi 6 Color' =>  'ansi_6_color',
+    'Selection Color' =>  'selection_color',
+    'Ansi 12 Color' =>  'ansi_12_color',
+    'Foreground Color' =>  'foreground_color',
+    'Ansi 1 Color' =>  'ansi_1_color',
+}
+
+puts '{'
+colors.each_pair do |color, rgb|
+  puts "#{names[color]}: {red: '#{rgb['Red Component']}', green: '#{rgb['Green Component']}', blue: '#{rgb['Blue Component']}'},"
+end
+puts '}'
