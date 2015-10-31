df4a067176d39308220bf97bdeff7f860b1fc982
Joakim Erelt <joakim.erelt@gmail.com>
2015-10-31 12:04:09 +0100

    Add emacs haskell mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -27,6 +27,9 @@ re-downloaded in order to locate PACKAGE."
 (blink-cursor-mode -1)
 (setq evil-operator-state-cursor '(box "white"))
 
+(require-package 'haskell-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+
 (require-package 'clojure-mode)
 (require-package 'cider)
 
