26ec81126c7e50afc8252513debc902e194ca66d
Joakim Erelt <joakim@erelt.se>
2025-06-23 05:47:20 +0200

    Remove kitty
    Experienced some strange issues. Sometimes the terminal would just do
    nothing after issuing a command, pressing enter would "wake it" (it
    looked like the output was buffered and enter would flush it). Also had
    some problems while sshing. Error messages about non-working terminal,
    missing colors etc.

--- install-terminal.sh
+++ install-terminal.sh
@@ -3,8 +3,3 @@
 sudo pacman -S --noconfirm --needed ttf-hack alacritty
 ln -sf ~/.dotfiles/alacritty.toml ~/.alacritty.toml
 
-sudo pacman -S --noconfirm --needed ttf-hack kitty
-mkdir -p ~/.config/kitty
-ln -sf ~/.dotfiles/current-theme.conf ~/.config/kitty/current-theme.conf
-ln -sf ~/.dotfiles/kitty.conf ~/.config/kitty/kitty.conf
-
