9c44099a2106e59ae2ea24a0661ec2d604c5a3af
Joakim Erelt <joakim@erelt.se>
2025-08-06 14:45:41 +0200

    Some kde customizations

--- install-kde-plasma.sh
+++ install-kde-plasma.sh
@@ -9,9 +9,16 @@ sudo cp -u sddm-theme.conf /etc/sddm.conf.d/theme.conf
 
 sudo pacman -S --noconfirm --needed plasma-desktop dolphin kde-gtk-config \
   plasma-nm plasma-pa plasma-disks print-manager system-config-printer \
-  spectacle okular kscreen ark
+  spectacle okular kscreen ark kate
 
 kwriteconfig6 --file kcminputrc --group Keyboard --key RepeatDelay 200
 kwriteconfig6 --file kcminputrc --group Keyboard --key RepeatRate 30
-kwriteconfig6 --file kxkbrc --group Layout --key Options caps:escape
+kwriteconfig6 --file kxkbrc --group Layout --key Options caps:escape,grp:shifts_toggle
+kwriteconfig6 --file kxkbrc --group Layout --key LayoutList us,se
+kwriteconfig6 --file kxkbrc --group Layout --key Use true
+kwriteconfig6 --file kxkbrc --group Layout --key ResetOldOptions true
 
+lookandfeeltool -a org.kde.breezetwilight.desktop
+
+# TODO Customize lock screen mod+esc
+# TODO Map mod+d to krunner
